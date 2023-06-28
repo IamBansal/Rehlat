e098c61fd2a95128102fcc095cbae95b1fc8227c
IamBansal <abansal11ae@gmail.com>
2023-06-28 11:42:43 +0530

    Initial commit

---
+++ app/src/main/java/com/rehalt/rehlat/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rehalt.rehlat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rehalt.rehlat.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
